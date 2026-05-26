748aa4e642a796e6135d27de5e9550457bddd72a
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2026-05-26 15:34:06 +0300

    ci: deploy to orkunkocyigit.github.io/trickcal-tools via GHP

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/trickcal-tools/' : '/',
   plugins: [
     vue(),
     // Gzip 壓縮
